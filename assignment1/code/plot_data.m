d11f3d8411d3916bf61fcc34ffa670870f440ddc
Laura Baakman <laurabaakman@gmail.com>
2014-12-10 15:52:34 +0100

    Niewe plaatjes met en zonder rode lijn.

--- assignment1/code/plot_data.m
+++ assignment1/code/plot_data.m
@@ -1,12 +1,17 @@
-
+clear all; close all force; clc;
 % Ensure that the plot labels are sort of legible
 set(0,'DefaultAxesFontSize', 24);
 set(0,'defaultlinelinewidth', 2);
 
 %% Assignment a. (Workspace: Aa_N50_Nd500_Nmax1000)
 load('../workspace/Aa_N50_Nd500_Nmax1000');
-figure('name', 'Assignment a.');
-data_plot = plot(alphas, results, 'Marker', 'o', 'MarkerFaceColor', 'blue', 'MarkerSize', 10);
+data_plot = figure('name', 'Assignment a.');
+hold on
+    plot(alphas, results, 'Marker', 'o', 'MarkerFaceColor', 'blue', 'MarkerSize', 5);
+    qls = theoreticalQls(N, alphas);
+    plot(alphas, qls, 'Marker', 'd', 'MarkerSize', 5, 'Color', 'red', 'MarkerFaceColor', 'red');
+hold off
+
 axis([0.75 3.0 -0.25 1.25])
 xlabel('\alpha = N / d');
 ylabel('Q_{l.s.}');
@@ -16,8 +21,8 @@ clear all;
 %% Assignment b. All N in separate figures. (Workspace: Ab_N25-25-150_Nd75_Nmax250)
 load('../workspace/Ab_N25-25-150_Nd75_Nmax250');
 for i = 1:size(results, 1)
-   figure('name', sprintf('Assignment b. N = %d', NS(i)));
-   data_plot = plot(alphas, results(i, :), 'Marker', 'o', 'MarkerFaceColor', 'blue', 'MarkerSize', 10);
+   data_plot = figure('name', sprintf('Assignment b. N = %d', NS(i)));
+    plot(alphas, results(i, :), 'Marker', 'o', 'MarkerFaceColor', 'blue', 'MarkerSize', 5);
    axis([0.75 3.0 -0.25 1.25]);
    xlabel('\alpha = N / d');
    ylabel('Q_{l.s.}');
