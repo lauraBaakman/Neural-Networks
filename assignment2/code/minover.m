d59977f3480dacc8be9fdcd1b29c3187fd211767
Laura Baakman <laurabaakman@gmail.com>
2015-01-05 09:00:49 +0100

    Start minover functie.

---
+++ assignment2/code/minover.m
@@ -0,0 +1,9 @@
+function [] = minover(data, labels, P, n_max)
+        %MINOVER algorithm to train a perceptron.
+        %   INPUT
+        %   DATA: D x N matrix with D vectors of dimension N.
+        %   LABELS: D X 1 matrix with D labels, that are either 0 or 1.
+        %   P: The number of times the weight vector should not change
+        %   before terminating the algorithm.
+        %   N_MAX: The number of 'sweeps' to perform, the total number of
+        %   steps is N_MAX x P.
