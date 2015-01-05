0f492d78e292b6856d11e3bced3c32fce7480ffe
Laura Baakman <laurabaakman@gmail.com>
2015-01-05 13:52:06 +0100

    Vergelijkingsfunctie heeft een error.

--- assignment2/code/minover.m
+++ assignment2/code/minover.m
@@ -1,10 +1,13 @@
-function [ weights, generalization_errors ] = minover(data, labels, n_max, w_star)
+function [ weights, generalization_errors ] = minover(data, labels, n_max, w_star, error)
     %MINOVER algorithm to train a perceptron.
     %   INPUT
     %   DATA: P x N matrix with P vectors of dimension N.
     %   LABELS: P X 1 matrix with P labels, that are either 0 or 1.
     %   N_MAX: The number of 'sweeps' to perform, the total number of
     %   steps is N_MAX x P.
+    %   W_STAR: The teacher weights
+    %   ERROR: The acceptable difference between weights when testing for
+    %   convergence.
     
     P = size(data, 1);
     N = size(data, 2);
@@ -18,7 +21,7 @@ function [ weights, generalization_errors ] = minover(data, labels, n_max, w_sta
     
     generalization_errors = zeros(t_max, 1);
     
-    while not_converged(weight_storage, P, N) && t < t_max
+    while not_converged(weight_storage, P, error) && t < t_max
         t = t + 1;
         mu = find_example_with_lowest_stability(data, labels, weights);
         current_pattern = data(mu, :);
@@ -36,8 +39,9 @@ function [idx] = find_example_with_lowest_stability(data, labels, weights)
     [~, idx] = min(stabilities);
 end
 
-function [ret] = not_converged(weight_storage, P, N)
-   ret = sum(sum(weight_storage == repmat(weight_storage(1, :), P, 1))) ~= P * N;
+function [ret] = not_converged(weight_storage, P, error)
+   difference = abs(weight_storage - repmat(weight_storage(1, :), P, 1));
+   ret = sum(sum(difference > error));
 end
 
 function [ret] = generalization_error(w, w_star)
