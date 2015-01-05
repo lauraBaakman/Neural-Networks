a9512bdd105aa8655df52712cc294be5486bbbfe
Rick van Veen <r.van.veen133@gmail.com>
2015-01-05 12:23:36 +0100

    Minover generalization errors

--- assignment2/code/main.m
+++ assignment2/code/main.m
@@ -3,17 +3,22 @@ clear all; close all force; clc;
 addpath('../../assignment1/code');
 
 %% Init
-D = 200;
+P = 250;
 N = 10;
-n_max = 100;
-P = 5;
+n_max = 200;
 
 %% Generate data and compute labels
-[data, ~] = generate_data(D, N);
+[data, ~] = generate_data(P, N);
 w_star = ones(1, N);
 labels = w_star * data';
 labels(labels > 0) = 1;
 labels(labels < 0) = -1;
 
 %% Min-over
-minover(data, labels', P, n_max);
+[weight, g_errors] = minover(data, labels', n_max, w_star);
+
+c_labels = weight * data';
+c_labels(c_labels > 0) = 1;
+c_labels(c_labels < 0) = -1;
+
+1 - sum(c_labels == labels) / P
--- assignment2/code/minover.m
+++ assignment2/code/minover.m
@@ -1,31 +1,32 @@
-function [] = minover(data, labels, P, n_max)
+function [ weights, generalization_errors ] = minover(data, labels, n_max, w_star)
     %MINOVER algorithm to train a perceptron.
     %   INPUT
-    %   DATA: D x N matrix with D vectors of dimension N.
-    %   LABELS: D X 1 matrix with D labels, that are either 0 or 1.
-    %   P: The number of times the weight vector should not change
-    %   before terminating the algorithm.
+    %   DATA: P x N matrix with P vectors of dimension N.
+    %   LABELS: P X 1 matrix with P labels, that are either 0 or 1.
     %   N_MAX: The number of 'sweeps' to perform, the total number of
     %   steps is N_MAX x P.
     
-    D = size(data, 1);
+    P = size(data, 1);
     N = size(data, 2);
     t_max = n_max * P;
     t = 0;
     p = 1;
     
-    weights = ones(1, N);
-    weight_storage = zeros(P, N);
+    weights = zeros(1, N);
+    weight_storage = ones(P, N);
     weight_storage(p, :) = weights;
     
-    while not_converged(weight_storage) && t < t_max
-        t = 1 + 1;
+    generalization_errors = zeros(t_max, 1);
+    
+    while not_converged(weight_storage, P, N) && t < t_max
+        t = t + 1;
         mu = find_example_with_lowest_stability(data, labels, weights);
         current_pattern = data(mu, :);
-        current_label = labels(mu, :);
-        weights = weights + 1/n * current_pattern * current_label;
-        p = mod(p, P + 1) + 1;
-        weight_storage(p, :) = weights;
+        current_label = labels(mu, :);        
+        weights = weights + 1/N * current_pattern * current_label;
+        generalization_errors(t) = generalization_error(weights, w_star);
+        p = mod(p, P) + 1;
+        weight_storage(p, :) = weights; 
     end
 end
 
@@ -35,6 +36,10 @@ function [idx] = find_example_with_lowest_stability(data, labels, weights)
     [~, idx] = min(stabilities);
 end
 
-function [ret] = not_converged(weight_storage)
+function [ret] = not_converged(weight_storage, P, N)
+   ret = sum(sum(weight_storage == repmat(weight_storage(1, :), P, 1))) ~= P * N;
+end
 
+function [ret] = generalization_error(w, w_star)
+    ret = 1 / pi * acos(w * w_star' / (norm(w) * norm(w_star)));
 end
