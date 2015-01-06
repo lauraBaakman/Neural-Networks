c2c31d6fa3d6df71af3cf5ba4ba4565717914435
Laura Baakman <laurabaakman@gmail.com>
2015-01-06 18:10:40 +0100

    Added font size for the legend to avoid legend overlapping the plot.

--- assignment2/code/main.m
+++ assignment2/code/main.m
@@ -6,16 +6,16 @@ addpath('../../assignment1/code');
 set(0,'DefaultAxesFontSize', 24);
 set(0,'defaultlinelinewidth', 2)
 
-matlabpool('open', 4);
+% matlabpool('open', 4);
 
 %% Init
-N = 10;
+N = 5;
 n_max = 500;
-n_d = 10;
+n_d = 1;
 error = 0.0005;
 
-alphas = 0.1:0.1:5.0;
-% alphas = 0.5:0.5:2;
+% alphas = 0.1:0.1:5.0;
+alphas = 0.5:0.5:2;
 
 %% Generate data and compute labels
 w_star = ones(1, N);
@@ -24,7 +24,7 @@ g_errors = zeros(max(alphas) * N * n_max, size(alphas, 2));
 final_g_errors = zeros(size(alphas,2), 1);
 
 %% Min-over
-parfor idx=1:size(alphas, 2)
+for idx=1:size(alphas, 2)
     P = ceil(alphas(idx) * N);
     g_errors_temp = zeros(P * n_max, n_d);
     for it = 1:n_d
@@ -45,27 +45,28 @@ parfor idx=1:size(alphas, 2)
 %         1 - sum(c_labels == labels) / P
     end
     final_g_errors(idx) = mean(final_g_error_temp);
-%     g_errors(1:P * n_max, idx) = nanmean(g_errors_temp, 2);
-%     g_errors(P * n_max + 1:end, idx) = NaN;    
+    g_errors(1:P * n_max, idx) = nanmean(g_errors_temp, 2);
+    g_errors(P * n_max + 1:end, idx) = NaN;    
 end
 
-matlabpool('close');
+% matlabpool('close');
 
 %% Plot 1
 
-% % Get a subset of the data
-% x = 1:2:size(g_error, 1);
-% g_errors_to_plot = g_errors(x, :);
-% 
-% % Remove NaNs
-% g_errors_to_plot( all( isnan( g_errors_to_plot ), 2 ), : ) = [];
-% 
-% fig = figure();
-% semilogx(g_errors_to_plot);
-% xlabel('3 * t')
-% ylabel('generalization error')
-% legend(cellstr(num2str(alphas', '%.2f')))
-% % saveas(fig, '../report/img/N5NMAX500error3.png');
+% Get a subset of the data
+x = 1:2:size(g_error, 1);
+g_errors_to_plot = g_errors(x, :);
+
+% Remove NaNs
+g_errors_to_plot( all( isnan( g_errors_to_plot ), 2 ), : ) = [];
+
+fig = figure();
+semilogx(g_errors_to_plot);
+xlabel('3 * t')
+ylabel('generalization error')
+h_leg = legend(cellstr(num2str(alphas', '%.2f')))
+set(h_leg,'FontSize',18);
+saveas(fig, '../report/img/N5NMAX500error3.png');
 
 %% Plot 2
 
