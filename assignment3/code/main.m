e3efc26e3d4e3c1a23f6a892052e4c829b5b5194
Laura Baakman <laurabaakman@gmail.com>
2015-01-13 14:45:55 +0100

    Plaatjes.

--- assignment3/code/main.m
+++ assignment3/code/main.m
@@ -1,12 +1,48 @@
 %% Start with a clean slate
 clear all; close all force; clc;
 
+% Defaults
+set(0,'DefaultAxesFontSize', 24);
+set(0,'defaultlinelinewidth', 4);
+set(0,'defaultAxesFontName', 'CMU Serif Roman')
+set(0,'defaultTextFontName', 'CMU Serif Roman')
+
 %% INIT
 load('data3.mat');
 learning_rate = 0.05;
 t_max = 200;
-num_training_samples = 100;
 
-%% Training
-training_data = datasample(xi, num_training_samples, 2);
-weights = stochastic_gradient_descent(training_data, tau, learning_rate, t_max);
+train_ratio = 0.4;
+test_ratio = 0.4;
+
+%% Subset data
+[train_ind, ~, test_ind] = dividerand(1:size(xi, 2), train_ratio, 1 - train_ratio - test_ratio, test_ratio);
+
+data_training = xi(:, train_ind);
+labels_training = tau(train_ind);
+
+data_test = xi(:, test_ind);
+labels_test = tau(test_ind);
+
+%% Train/Test
+[weights, cost, generalization_error] = stochastic_gradient_descent(data_training, labels_training, data_test, labels_test, learning_rate, t_max);
+
+%% Plot Errors
+h = figure('name', 'threshold');
+plot(1:t_max, cost, 1:t_max, generalization_error)
+h_legend = legend('Cost', 'Generalization Error');
+set(h_legend, 'FontSize',14, 'Orientation','horizontal', 'Location', 'north');
+xlabel('time');
+ylabel('error')
+saveas(h, sprintf('../report/img/errors_train_%d_test_%d.png', size(data_training, 2), size(data_test, 2)));
+
+%% Plot weights
+h = figure('name', 'weights');
+bar(weights, 'r');
+set(gca,'XTick',1:2);
+set(gca,'XTickLabel',['w1'; 'w2']);
+saveas(h, sprintf('../report/img/weights_train_%d_test_%d.png', size(data_training, 2), size(data_test, 2)));
+
+%% Store mat file
+dat = datestr(now,'yyyymmdd-HHMMSS');
+save(sprintf('train_%d_test_%d_%s.mat', size(data_training, 2), size(data_test, 2), dat));
--- assignment3/code/stochastic_gradient_descent.m
+++ assignment3/code/stochastic_gradient_descent.m
@@ -1,28 +1,39 @@
-function [ weights ] = stochastic_gradient_descent( data, labels, learning_rate, t_max)
+function [ weights, cost, generalization_error ] = stochastic_gradient_descent( data_training, labels_training, data_test, labels_test, learning_rate, t_max)
     %STOCHASTIC_GRADIENT_DESCENT Summary of this function goes here
     %   INPUT
-    %       DATA:           N X P matrix with P N-dimensional vectors.
-    %       LABELS:         1 X P matrix with continous labels
+    %       DATA_q:           N X P matrix with P N-dimensional vectors.
+    %       LABELS_q:         1 X P matrix with continous labels    
     %       LEARNING_RATE:  The learning rate
     %       T_MAX:          THe algorithm performs P * t_max training steps
     %   OUTPUT
     %       WEIGHTS         2 x N matrix with the first and second layer
     %       weights.
     
-    P = size(data, 2);
-    N = size(data, 1);
-    total_number_of_steps = P * t_max;
-    t = 0;
+    P = size(data_training, 2);
+    N = size(data_training, 1);
     
     weights = initialize_weights(N);
     
-    while t < total_number_of_steps
-        [pattern, idx] = datasample(data, 1, 2);
-        tau = labels(idx);
-        weights = update_weights(pattern, weights, tau, learning_rate);
-        t = t + 1;
+    cost = ones(t_max, 1);
+    generalization_error = ones(t_max, 1);
+    
+    for t = 1:t_max
+        for i = 1:P
+            [pattern, idx] = datasample(data_training, 1, 2);
+            tau = labels_training(idx);
+            weights = update_weights(pattern, weights, tau, learning_rate);
+        end
+        cost(t) = compute_error(data_training, labels_training, weights);
+        generalization_error(t) = compute_error(data_test, labels_test, weights);
     end
     
+end
+
+function [error] = compute_error(data, labels, weights)
+    P = size(data, 2);
+    sig = @(pattern) sigma(pattern, weights);
+    sigmas = cell2mat(cellfun(sig, num2cell(data, 1), 'UniformOutput', false));
+    error = sum((sigmas - labels).^2) * 0.5 * 1/P;
 end
 
 function [weights] = update_weights(pattern, weights, tau, learning_rate)
@@ -35,7 +46,7 @@ end
 function [w] = update_weight(weight, learning_rate, sigma_min_tau, c, pattern)
     w = weight - learning_rate * gradient(sigma_min_tau, weight, pattern) * c;
 end
-        
+
 
 function [c] = contribution(sigma_min_tau)
     c = (sigma_min_tau^2)/2;
@@ -46,7 +57,7 @@ function [g] = gradient(sigma_min_tau, weight, pattern)
 end
 
 function [s] = sigma(pattern, weights)
-   s = tanh(weights(1, :) * pattern) + tanh(weights(2, :) * pattern); 
+    s = tanh(weights(1, :) * pattern) + tanh(weights(2, :) * pattern);
 end
 
 function [weights] = initialize_weights(N)
@@ -55,7 +66,7 @@ function [weights] = initialize_weights(N)
 end
 
 function [weight] = initialize_weight(N)
-   weight = rand(1, N);
-   weight = sqrt((weight .^2) / sum(weight.^2));
+    weight = rand(1, N);
+    weight = sqrt((weight .^2) / sum(weight.^2));
 end
 
