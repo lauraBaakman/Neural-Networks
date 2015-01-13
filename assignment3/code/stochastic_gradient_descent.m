c8d2781c5eb2c7e74e0d9e09a9ad3bd9304d06a2
Laura Baakman <laurabaakman@gmail.com>
2015-01-13 11:38:38 +0100

    Parameter toegvoegd.

--- assignment3/code/stochastic_gradient_descent.m
+++ assignment3/code/stochastic_gradient_descent.m
@@ -1,9 +1,10 @@
-function [ output_args ] = stochastic_gradient_descent( data, labels, learning_rate)
+function [ output_args ] = stochastic_gradient_descent( data, labels, learning_rate, t_max)
     %STOCHASTIC_GRADIENT_DESCENT Summary of this function goes here
     %   INPUT
     %       DATA:           N X P matrix with P N-dimensional vectors.
     %       LABELS:         1 X P matrix with continous labels
     %       LEARNING_RATE:  The learning rate
+    %       T_MAX:          THe algorithm performs P * t_max training steps
     %   OUTPUT
     %       
     
